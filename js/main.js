4ca7c18a8bab2df35a0f4eb3b0d8d9e500f23bc6
Mahmoud Tayel <mhmodtayel0050@gmail.com>
2021-12-14 01:28:09 +0200

    add about me section

--- js/main.js
+++ js/main.js
@@ -1,5 +1,23 @@
-$(document).ready(() => {
+// $(document).ready(() => {
 
-  
+// })
 
-})
+// function calc(oprator, result) {
+//   let num1 = 0,
+//     num2 = 0;
+//   if (oprator == "*") {
+//     num1 = 1;
+//     num2 = 1;
+//     num1Arr = [];
+//     for (let i = 1; i <= result; i++) {
+//       if (result % i == 0) num1Arr.push(i);
+//     }
+//     return num1Arr;
+//   }else if (oprator == "+") {
+//     for (let i = 1; i <= result; i++) {
+//       if (result % i == 0) num1Arr.push(i);
+//     }
+//   }
+// }
+
+// console.log(calc("*", 200));
